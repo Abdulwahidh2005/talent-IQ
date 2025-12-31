833590f4132d9a4df399119fb0a0595a36c1c53b
Abdulwahdih2005 <abdulwahidh@2005>
2025-12-31 11:09:15 +0530

    db and tools setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import { ENV } from './env.js';
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(ENV.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`✅MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌Error: ${error.message}`);
+    process.exit(1);
+  }
+}
